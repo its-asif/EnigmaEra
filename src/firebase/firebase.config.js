f6e7cd4321b8880bda1dda368a52e2c2f1056ceb
its-asif <asifiscoding@gmail.com>
2023-10-08 16:05:28 +0600

    firebase added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAbO87Xc8WMV02tpmR6rDiPttIYYaWmN84",
+  authDomain: "enigma-era.firebaseapp.com",
+  projectId: "enigma-era",
+  storageBucket: "enigma-era.appspot.com",
+  messagingSenderId: "117150374013",
+  appId: "1:117150374013:web:51922c3e754705705bb914"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
